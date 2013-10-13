471640b7bb0a24bc51d32216a6007bbd7e6e07b2
Andrew Darqui <andrew.darqui@gmail.com>
2013-10-13 04:10:22 -0400

    cap first char mod

--- app.js
+++ app.js
@@ -4,6 +4,10 @@
 
 var
   Input = require('./lib/input.js'),
+  Rooms = require('./lib/rooms.js'),
+  Users = require('./lib/users.js'),
+  Title = require('./lib/title.js'),
+  Chat = require('./lib/chat.js'),
   wobot = require('wobot'),
   irc = require('irc'),
   blessed = require('blessed'),
@@ -18,6 +22,10 @@ var init = {
   everything : function() {
     console.log(Input)
     c.input = new Input(c)
+    c.rooms = new Rooms(c)
+    c.users = new Users(c)
+    c.title = new Title(c)
+    c.chat = new Chat(c)
     setInterval(function() { } , 2000)
   }
 }
